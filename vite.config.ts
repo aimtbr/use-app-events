ad6a2a7f4dda065daa987b1dc8526df7181e99e4
Maksym Marchuk <maxim.marchuk.d.corp@gmail.com>
2025-02-25 21:25:16 +0200

    Update the outdated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import path from 'path';
-// import { rename } from 'fs/promises';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
